2241a49e86d69fed40bdd013f859d1f7eb67d52c
Rahul Jadhav <jadhav.rj.cmos@gmail.com>
2023-07-16 11:08:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ const App = () => {
       <h1 class="count" onDoubleClick={() => { alert("cant edit it") }}>{count}</h1>
       <button class='increment-button' onClick={() => { setCount(count + 1) }}>Increment</button>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
